3ebcf28306ef1a42d7924552be82e7ff2484324b
bigmack2304 <37216622+bigmack2304@users.noreply.github.com>
2022-05-08 17:36:40 +0300

    update 3

--- js/qr_descriptor.js
+++ js/qr_descriptor.js
@@ -1,73 +1,8 @@
 'use strict';
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-служебный класс, бозволяет засечь время выполнения определенного участка кода
-ИСПОЛЬЗОВАНИЕ
-
-    const timer1 = new addon_CodeTime("timer1");     // создаем счетчик с именем timer1
-
-    timer1.start();         // запускаем таймер
-    ...                     // выполняем код
-    timer1.end_get_ms();    // останавливаем таймер, выводим результат в милисекундах в консоль
-    timer1.end_get_sec();   // останавливаем таймер, выводим результат в секундах в консоль 
-
-
-    Функции start и end могут быть вызвоны только 1 раз (всмысле их бесполезно вызывать много раз подрят)
-    они могут работать только поочередно (start -> end)
-
-*/
-
-class addon_CodeTime {
-    #name;              // имя таймера
-    #isStart;           // влаг старта\конца
-
-    constructor(name) {
-        this.#name = name;
-    }
-
-    start() {                           // запускаем таймер
-        if (!this.#isStart) {
-            this.#isStart = true;
-            this.time_start = performance.now();
-        } else {
-            console.log("ERR. " + this.#name + ": is started")
-        }
-    }
-
-    end_get_ms() {                             // завершаем таймер, выводим результат в консоль в милисекундах
-        if (this.#isStart) {
-            this.time_end = performance.now();
-            console.log(this.#name + ": " + (this.time_end - this.time_start) + " ms");
-            this.#isStart = false;
-        } else {
-            console.log("ERR. " + this.#name + ": is not started")
-        }
-    }
-
-    end_get_sec() {                     // завершаем таймер, выводим результат в консоль в секундах
-        if (this.#isStart) {
-            this.time_end = performance.now();
-            console.log(this.#name + ": " + ((this.time_end - this.time_start) / 1000) + " sec");
-            this.#isStart = false;
-        } else {
-            console.log("ERR. " + this.#name + ": is not started")
-        }
-    }
-}
-
-// const t1 = new addon_CodeTime("1");
-// t1.start();
-// t1.end_get_sec();
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 let file_txt;                                                                           // будет содержать в себе временный текст из загруженного фаила
+let pages = 0;
+let ned_pages_dop = 0;
 
 const qr_info = document.getElementsByClassName("file_info")[0];                        // блок с инфой о фаиле
 qr_info.style.display = 'none';                                                         // лучше сразу его скроем (по умолчанию)
@@ -122,7 +57,9 @@ function file_input(obj) {                                                      
 
         btn_active_deactive(btn_filter[0], false);              // включаем нажимаемость для кнопок
         btn_active_deactive(btn_decomp[0], false);
-        btn_active_deactive(btn_download[0], false);      
+        btn_active_deactive(btn_download[0], false);     
+        pages = 0; 
+        ned_pages_dop = 0;
     }
 
     reader.onerror = () => {
@@ -154,7 +91,7 @@ function btn_decomp_klick() {
     if (file_txt != undefined) {
         const need_qr = document.getElementsByClassName("btn_decomp_n")[0].value;       // сколько должно быть qr кодов в фаиле
         const need_docs = Math.ceil(Number(qr_size_n.innerText)/need_qr);               // щитаем сколько нам понадобится страниц
-
+        ned_pages_dop = need_docs;
         file_wath[0].innerHTML = "";
         let file_txt_i = 0;
 
@@ -177,6 +114,7 @@ function btn_decomp_klick() {
             }
             file_check_final(arry_temp);
         }
+   //     addon_spoiler_start_script();
     } else {
         alert("Нужно сначала открыть фаил");
     }
@@ -187,7 +125,8 @@ function btn_decomp_klick() {
 }
 
 function btn_download_klick() {
-    let pages = document.getElementsByClassName("text_in_file__text");
+   // let pages = document.getElementsByClassName("text_in_file__text");
+   let pages = document.getElementsByClassName("final_page");
     let zip = new JSZip();                     // создаем новый архив                
 
     for (let i=0; i < pages.length; i++) {
@@ -249,6 +188,7 @@ function file_check(input) {
 
 }
 
+// заполняем окно просмотра разделенными страницами с содержанием
 function file_check_final(input) {
     const thread_3 = new Promise((resolve, reject) => {
         let temp = "";
@@ -262,15 +202,25 @@ function file_check_final(input) {
     .then((data) => {
         let list = document.createElement('div');
         list.className = "text_in_file__container";
-        list.innerHTML = '<plaintext class= "text_in_file__text">';
+        list.innerHTML = '<div class="js-addon_spoiler"> <div class="addon_spoiler_heder" tabindex="0"> <p></p> <div class="addon_spoiler_indicator"></div> </div> <div class="addon_spoiler_body"> <plaintext class="final_page">';
         file_wath[0].append(list); 
-        let text_cotainer = file_wath[0].lastChild.children[0];
+        let text_cotainer = file_wath[0].lastChild.children[0].children[1].children[0];
         text_cotainer.innerHTML = data;
+        text_cotainer = file_wath[0].lastChild.children[0].children[0].children[0];
+        text_cotainer.innerHTML = 'Document:' + (pages + 1) + ' codes:' + ((data.match(new RegExp("\n", "g")) || []).length);
+        pages++;
+
+        //(data.match(new RegExp("\n", "g")) || []).length
+        // нельзя использовать addEventListener с одним и темже событием для одного и тогоже обьекта несколько раз
+        // иначе оно будет сробатывать несколько раз!
+        if (ned_pages_dop == pages) {
+            addon_spoiler_start_script();
+            qr_info.style.display = 'none';      
+        }
     })
     .catch((error) => {
-        console.log("thread_2 error");
+        console.log("thread_3 error");
     });
-
 }
 
 // активатор кнопок (обьект кнопки, true = деактевировать)
@@ -304,241 +254,3 @@ function roughSizeOfObject(object) {
     }
     return bytes;
 }
-
-
-
-
-
-/*
-
-
-'use strict';
-
-let file_txt;                                                                           // будет содержать в себе временный текст из загруженного фаила
-
-const qr_info = document.getElementsByClassName("file_info")[0];                        // блок с инфой о фаиле
-qr_info.style.display = 'none';                                                         // лучше сразу его скроем (по умолчанию)
-
-const qr_size_n = document.getElementsByClassName("file_info__QRn")[0].children[0];            // сюда записать кол/во qr кодов (строк) в фаиле
-const doc_size_n = document.getElementsByClassName("file_info__size")[0].children[0];          // инфа о размере фаила (для прикола)
-const file_wath = document.getElementsByClassName("text_in_file");                             // окно, в котором можно посмотреть фаил
-
-const btn_filter = document.getElementsByClassName("btn_filter_qr");            // кнопка удалить лишнее
-if (btn_filter.length >= 1) {
-    btn_filter[0].addEventListener ('click', (evt) => {                       
-        btn_filter_klick(evt);
-    });    
-}
-
-const btn_decomp = document.getElementsByClassName("btn_decomp");               // кнопка разбить на фаилы
-if (btn_decomp.length >= 1) {
-    btn_decomp[0].addEventListener ('click', (evt) => {                         
-        btn_decomp_klick(evt);
-    });    
-}
-
-const btn_download = document.getElementsByClassName("btn_download");           // кнопка скачать
-if (btn_download.length >= 1) {
-    btn_download[0].addEventListener ('click', (evt) => {                          
-        btn_download_klick(evt);
-    });    
-}
-
-// выключаем нажимаемость для определенных кнопок
-btn_active_deactive(btn_filter[0], true);
-btn_active_deactive(btn_decomp[0], true);
-btn_active_deactive(btn_download[0], true);
-
-const reader = new FileReader();                                                // обьект для работы с фаилом
-const file_uploader = document.getElementsByClassName("js-file_uploader");      // получаем хагрузчик фаила
-if (file_uploader.length >= 1) {
-    file_uploader[0].addEventListener ('change', (evt) => {                     // вешаем на него событие загрузки
-        file_input(evt);
-    });
-}
-
-function file_input(obj) {                                                      // вызывается при загрузке фаила
-    const file = obj.target.files[0];
-    reader.readAsText(file);
-
-    reader.onload = () => {
-        file_txt = reader.result;                               // считанные данные пишем в переменную
-        file_txt = file_txt.split("\n");                        // преобразуем в массив, строка - ячейка
-        file_check(file_txt);                                   // обновляем содержимое в окне просмотра
-        qr_info.style.display = 'block';                        // делаем видимым блок информации о фаиле
-
-        btn_active_deactive(btn_filter[0], false);              // включаем нажимаемость для кнопок
-        btn_active_deactive(btn_decomp[0], false);
-        btn_active_deactive(btn_download[0], false);       
-    }
-
-
-    reader.onerror = () => {
-        alert("Ошибка чтения фаила");
-    }
-
-}
-
-
-
-
-function btn_filter_klick () {                                              // удалить лишнее
-    if (file_txt != undefined) {
-       // обновить переменную, тк в ней почемуто не сохранились переносы мтрок
-       file_txt = document.getElementsByClassName("text_in_file__text")[0].innerHTML;
-       file_txt = file_txt.split("\n");   
-       // обрабатываем каждую строку
-        for (let i=0; i < file_txt.length; i++) {
-            file_txt[i] = file_txt[i].slice(0, is_tab(file_txt[i]));        // удаляем все после таба
-            if (file_txt[i] == '') {
-                file_txt.splice(i);
-            }
-        }
-
-       file_check(file_txt);
-    } else {
-        alert("Нужно сначала открыть фаил");
-    }
-}
-
-function btn_decomp_klick() {
-    if (file_txt != undefined) {
-        const need_qr = document.getElementsByClassName("btn_decomp_n")[0].value;       // сколько должно быть qr кодов в фаиле
-        const need_docs = Math.ceil(Number(qr_size_n.innerText)/need_qr);               // щитаем сколько нам понадобится страниц
-
-//        file_txt = document.getElementsByClassName("text_in_file__text")[0].innerHTML;  // записываем в переменную file_txt, текст из окна просмотра,
-//        file_txt = file_txt.split("\n"); 
-
-        file_wath[0].innerHTML = "";
-        let file_txt_i = 0;
-
-        for (let i=0; i < need_docs; i++) {
-            let arry_temp = [];
-            for (let k=0; k < need_qr; k++) {
-                if (file_txt[file_txt_i] != undefined) {
-                    arry_temp.push(file_txt[file_txt_i]);
-                    file_txt_i++;
-
-                    if (file_txt[file_txt_i] != '') {
-                        //  file_txt[i] = file_txt[i].concat("\n");
-                      } else {
-                          file_txt.splice(i);
-                      }
-
-                } else {
-                    break;
-                }
-            }
-            file_check_final(arry_temp);
-        }
-    } else {
-        alert("Нужно сначала открыть фаил");
-    }
-
-    btn_active_deactive(btn_filter[0], true);
-    btn_active_deactive(btn_decomp[0], true);
-    btn_active_deactive(btn_download[0], false);
-}
-
-function btn_download_klick() {
-    let pages = document.getElementsByClassName("text_in_file__text");
-    let zip = new JSZip();                     // создаем новый архив                
-
-    for (let i=0; i < pages.length; i++) {
-	    let text = pages[i].innerText;
-        if (text.endsWith("\n")) {
-            text =text.slice(0, text.length-1);
-        }
-
-        zip.file('QR_page_' + i + '.txt', text);
-    }
-
-     zip.generateAsync({type:"blob"}).then(function(content) {
-        saveAs(content, "QR_pages.zip");
-    });
-
-}
-
-function is_tab(str) {
-    // возвращать индекс табудяции, либо конец строки
-    let idx = str.indexOf('\t');
-    if (idx != -1) {
-        return idx;
-    } else {
-        return (str.length);
-    }
-}
-
-function is_space(str) {
-    // возвращать индекс пробела, либо конец строки
-    let idx = str.indexOf('\ ');
-    if (idx != -1) {
-        return idx;
-    } else {
-        return (str.length);
-    }
-}
-
-function file_check(input) {
-   file_wath[0].innerHTML = "";
-   file_wath[0].innerHTML = '<div class="text_in_file__container"><plaintext class= "text_in_file__text">'
-   let text_cotainer = document.getElementsByClassName("text_in_file__text");
-   for (let i=0; i < input.length; i++) {
-    text_cotainer[0].innerHTML += input[i].concat("\n");
-   }
-   qr_size_n.innerHTML = input.length;
-   doc_size_n.innerHTML = "~ " + roughSizeOfObject(input) / 1000 + " kb";
-}
-
-function file_check_final(input) {
-    let list = document.createElement('div');
-    list.className = "text_in_file__container";
-    list.innerHTML = '<plaintext class= "text_in_file__text">';
-    file_wath[0].append(list); 
-    let text_cotainer = file_wath[0].lastChild.children[0];
-    for (let i=0; i < input.length; i++) {
-        text_cotainer.innerHTML += input[i].concat("\n");
-    }
-}
-
-// активатор кнопок (обьект кнопки, true = деактевировать)
-function btn_active_deactive(btn, mode) {
-    btn.disabled = mode;
-    (mode == true) ? (btn.classList.add("deactivated")) : (btn.classList.remove("deactivated"));
-} 
-
-
-
-// возвращает размер обьекта
-function roughSizeOfObject( object ) {
-    var objectList = [];
-    var stack = [ object ];
-    var bytes = 0;
-    while ( stack.length ) {
-        var value = stack.pop();
-        if ( typeof value === 'boolean' ) {
-            bytes += 4;
-        }
-        else if ( typeof value === 'string' ) {
-            bytes += value.length * 2;
-        }
-        else if ( typeof value === 'number' ) {
-            bytes += 8;
-        }
-        else if
-        (
-            typeof value === 'object'
-            && objectList.indexOf( value ) === -1
-        )
-        {
-            objectList.push( value );
-            for( var i in value ) {
-                stack.push( value[ i ] );
-            }
-        }
-    }
-    return bytes;
-}
-
-
-*/
---
+++ js/s_addon_spoiler.js
@@ -0,0 +1,105 @@
+'use strict';
+
+//addon spoiler
+const addon_spoiler_use_one = false;    // при открытии одного споилера, закрывать остальные открытые
+const addon_spoiler_def_hide = true;    // все споилеры закрыты по умолчанию       
+const addon_spoiler_resize_upd = true;  // обновлять размер открытого спойлера при изменении размеров окна браузера         
+
+
+//addon_spoiler_start_script();
+function addon_spoiler_start_script() {
+
+// проверяет обьекты возвращаемые от getElementsByClassName
+// если там есть хоть 1 элемент, вернет true
+const is_object_valid = (obj) => {
+    return (obj.length != 0) ? (true) : (false);
+}
+
+let get_addon_spoilers = document.getElementsByClassName('js-addon_spoiler');  // получить все закрытые спойлеры
+if (!addon_spoiler_def_hide && is_object_valid(get_addon_spoilers)) { 
+  for(let i=0; i < get_addon_spoilers.length; ) {                                // обходим все найденные спойлеры  
+	addon_spoiler_use(get_addon_spoilers[i]); 
+  }
+}
+
+let addon_spoilers = document.getElementsByClassName('addon_spoiler_heder');  // получить все заголовки спойлеров
+if (is_object_valid(addon_spoilers)) {
+  for(let i=0; i < addon_spoilers.length; i++) {                     
+	addon_spoilers[i].addEventListener('click', function (evt) {                // повесить на них обработчик "клик мышкой"
+		addon_spoiler_on_click(evt);
+	}); 
+
+	addon_spoilers[i].addEventListener('keydown', function(evt) {               // повесить на них обработчик "выбор enter,ом"
+		if (evt.code == 'Enter') addon_spoiler_on_click(evt);
+	});
+  } 
+}
+
+if (addon_spoiler_resize_upd && is_object_valid(get_addon_spoilers)) {
+  document.addEventListener('DOMContentLoaded', function(evt) {   // если DOM загрузился
+    window.addEventListener('resize', function(evt) {             // если мы меняем размер окна 
+      resize_upd();                                               // фызываем функцию
+    });
+  });
+}
+
+}
+
+function addon_spoiler_on_click(DOM_obj) {
+  let get_addon_spoiler_active = document.getElementsByClassName('js-addon_spoiler--active'); // получить все открытые спойлеры
+  let object = DOM_obj.currentTarget.parentElement;                 // получаем доступ к родительскому обьекту элемента addon_spoilers
+
+  if (addon_spoiler_use_one) {                                      // если разрешено использовать только 1 споилер  
+    for (let i=0; i < get_addon_spoiler_active.length; i++) {       // обходим все открытые споилеры
+      if (get_addon_spoiler_active[i] !== object) {                 // если это не тот споилер по которому был клик
+        addon_spoiler_use(get_addon_spoiler_active[i]);             // имитируем нажатие по нему (закрываем)
+      }
+    }
+  }
+  addon_spoiler_use(object);                                        // закрыть или открыть споилер
+}
+
+function addon_spoiler_use(DOM_obj) {
+  let is_active = (DOM_obj.classList.contains('js-addon_spoiler--active')) ? (true) : (false); // проверяет наличие нужного класса в списке, если он есть значит спойлер открыт
+  let object_body = DOM_obj.lastElementChild;                   // обьект с содержимым слайдера
+  let object_body_height = object_body.scrollHeight;            // размер содержимого слайдера, в пикселях
+  DOM_obj.classList.toggle("js-addon_spoiler");                 // удаляем у родителя класс если он был, если нет то добавляем
+  DOM_obj.classList.toggle("js-addon_spoiler--active");         // удаляем у родителя класс если он был, если нет то добавляем
+
+  if (!is_active) {                                             // если споилер закрыт
+    object_body.style.maxHeight = object_body_height + "px";    // делаем высату для содержимого, равной его размеру
+    upd_parent_height(DOM_obj,object_body_height);
+  } else {                                                      // если споилер открыт
+    object_body.style.maxHeight = "0px";                        // делаем высату для содержимого 0
+    upd_parent_height(DOM_obj,0);
+  }
+}
+
+function resize_upd() {
+  let get_addon_spoiler_active = document.getElementsByClassName('js-addon_spoiler--active');   // получить все открытые спойлеры
+  if (get_addon_spoiler_active != null) {                                                       // если активные споилеры есть
+    for (let i=0; i < get_addon_spoiler_active.length; i++) {                                   // обходим все открытые споилеры
+      let this_content = get_addon_spoiler_active[i].lastElementChild;                          // получаем обьект контента
+      let this_content_height = this_content.scrollHeight;                                      // получаем его высоту
+      this_content.style.maxHeight = this_content_height + "px";                                // задаем параметр максимальной высоты равным его текущей высоте
+    }
+  }
+}
+
+function check_parent_spoiler(obj) {
+  let parent_slider = obj.closest('.js-addon_spoiler--active');
+  if (parent_slider == null) {
+    return false;
+  }
+  return parent_slider;
+}
+
+function upd_parent_height(DOM_obj, old_size) {
+  let check_p_slider = DOM_obj;
+  let children_h = old_size;
+  while (check_parent_spoiler(check_p_slider.parentElement) != false) {
+    check_p_slider = check_parent_spoiler(check_p_slider.parentElement);
+    check_p_slider.lastElementChild.style.maxHeight = check_p_slider.lastElementChild.scrollHeight + children_h + "px";
+    children_h = check_p_slider.lastElementChild.scrollHeight + children_h;
+  }
+}

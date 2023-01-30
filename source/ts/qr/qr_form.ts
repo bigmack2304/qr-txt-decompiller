7d91465eddc172543500e02a30497d8bf0b54f5a
bigmack2304 <dumon7@yandex.ru>
2023-01-30 17:51:57 +0300

    Добавил валидацию формы

---
+++ source/ts/qr/qr_form.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+import { find_element } from "./qr_utils";
+
+function init() {
+    const form = find_element<HTMLInputElement>("js-btn_decomp_n");
+    form.addEventListener("change", form_change);
+}
+
+function form_change(e: Event) {
+    const target = e.target as HTMLInputElement;
+
+    if (Number(target.value) > Number(target.max)) {
+        target.value = target.max;
+    }
+
+    if (Number(target.value) < Number(target.min)) {
+        target.value = target.min;
+    }
+}
+
+export { init };
